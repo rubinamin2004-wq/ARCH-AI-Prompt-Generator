369882d297d45909a979d25b1a0be80f40ae2c60
rubinamin2004-wq <rubinamin2004@gmail.com>
2026-06-06 10:08:25 +0530

    Update app2.py

--- app2.py
+++ app2.py
@@ -324,7 +324,7 @@ with col_left:
     st.markdown('<span class="sec">00B — Gemini Model</span>', unsafe_allow_html=True)
     gemini_model = st.selectbox(
         "Gemini Model",
-        [
+        [   "gemini-3.1-flash-lite",
             "gemini-2.5-flash",
             "gemini-2.5-pro",
             "gemini-2.0-flash",
